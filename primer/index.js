34e04b931257b15a7c8c04e459694d29a2ba8e23
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-01 11:10:25 +0700

    Javascript doesn't differentiate between an unsigned value and a zero value in a logical OR operator.

--- primer/index.js
+++ primer/index.js
@@ -5,12 +5,12 @@ console.log(`Boots price ${bootsPrice}`)
 
 
 
-if (hatPrice == bootsPrice) {
+if (hatPrice === bootsPrice) {
 	console.log("Price are the same")
 } else {
 	console.log("Prices are diffrent")
 }
-let totalPrice = hatPrice+bootsPrice
+let totalPrice = Number(hatPrice) + Number(bootsPrice)
 console.log(`Total price: ${totalPrice}`);
 
 
@@ -18,3 +18,16 @@ let myVariable = "Adam"
 console.log(`Type: ${typeof myVariable}`);
 myVariable = 100
 console.log(`Type: ${typeof myVariable}`);
+
+
+
+let firstCity;
+let secondCity = firstCity || "London"
+//console.log(`City: ${secondCity}`)
+console.log(`City: ${firstCity || "London"}`)
+
+
+let taxRate; // no tax rate has been defined
+console.log(`Tax rate: ${taxRate || 10}%`)
+taxRate = 0
+console.log(`Tax rate: ${taxRate || 10}%`)
