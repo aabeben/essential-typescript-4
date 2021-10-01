ca97a1563f6f72d2315549800e713fe6d62614fe
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-01 20:21:02 +0700

    use default when invoked without a corresponding value.

--- primer/index.js
+++ primer/index.js
@@ -2,11 +2,16 @@ let hatPrice = 100
 console.log(`Hat price ${hatPrice}`)
 let bootsPrice = "100"
 console.log(`Boots price ${bootsPrice}`)
-function sumPrice(first, second, third) {
+function sumPrices(first, second, third=0) {
 	return first + second + third;
 }
-let totalPrice = sumPrice(hatPrice, bootsPrice);
-console.log(`Total Price: ${totalPrice}`);
+let totalPrices = sumPrices(hatPrice, bootsPrice);
+console.log(`Total: ${totalPrices} ${typeof totalPrices}`);
+totalPrices = sumPrices(100, 200, 300)
+console.log(`Total: ${totalPrices} ${typeof totalPrices}`)
+
+totalPrices = sumPrices(100,200)
+console.log(`Total: ${totalPrices} ${typeof totalPrices}`)
 
 
 
