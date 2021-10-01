1bab5891168f990eed31b1cc07bf864bdb0e92ed
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-01 11:03:26 +0700

    Initial commit

---
+++ primer/index.js
@@ -0,0 +1,20 @@
+let hatPrice = 100
+console.log(`Hat price ${hatPrice}`)
+let bootsPrice = "100"
+console.log(`Boots price ${bootsPrice}`)
+
+
+
+if (hatPrice == bootsPrice) {
+	console.log("Price are the same")
+} else {
+	console.log("Prices are diffrent")
+}
+let totalPrice = hatPrice+bootsPrice
+console.log(`Total price: ${totalPrice}`);
+
+
+let myVariable = "Adam"
+console.log(`Type: ${typeof myVariable}`);
+myVariable = 100
+console.log(`Type: ${typeof myVariable}`);
