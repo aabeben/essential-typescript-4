1d3f1c6d4e82f972dec9206d00eabc1c534a7e29
Beny Hasanudin <bhasanudin@gmail.com>
2021-10-01 20:29:11 +0700

    to ensure the function produces a useful sum of its parameters valeus however they are received, they can be converted and filtered to remove any that are NaN.

--- primer/index.js
+++ primer/index.js
@@ -4,7 +4,7 @@ let bootsPrice = "100"
 console.log(`Boots price ${bootsPrice}`)
 function sumPrices(...numbers) {
 	return numbers.reduce((total, val)=>{
-		return total+val;
+		return total+(Number.isNaN(Number(val))?0:Number(val));
 	},0);
 }
 let totalPrices = sumPrices(hatPrice, bootsPrice);
@@ -12,36 +12,36 @@ console.log(`Total: ${totalPrices} ${typeof totalPrices}`);
 totalPrices = sumPrices(100, 200, 300)
 console.log(`Total: ${totalPrices} ${typeof totalPrices}`)
 
-totalPrices = sumPrices(100,200)
+totalPrices = sumPrices(100,200, undefined,false, "hello")
 console.log(`Total: ${totalPrices} ${typeof totalPrices}`)
-totalPrices = sumPrices(100);
-console.log(`Total: ${totalPrices} ${typeof totalPrices}`);
+//totalPrices = sumPrices(100);
+//console.log(`Total: ${totalPrices} ${typeof totalPrices}`);
 
 
 
-if (hatPrice === bootsPrice) {
-	console.log("Price are the same")
-} else {
-	console.log("Prices are diffrent")
-}
+//if (hatPrice === bootsPrice) {
+	//console.log("Price are the same")
+//} else {
+	//console.log("Prices are diffrent")
+//}
 //let totalPrice = Number(hatPrice) + Number(bootsPrice)
 //console.log(`Total price: ${totalPrice}`);
 
 
-let myVariable = "Adam"
-console.log(`Type: ${typeof myVariable}`);
-myVariable = 100
-console.log(`Type: ${typeof myVariable}`);
+//let myVariable = "Adam"
+//console.log(`Type: ${typeof myVariable}`);
+//myVariable = 100
+//console.log(`Type: ${typeof myVariable}`);
 
 
 
-let firstCity;
-let secondCity = firstCity || "London"
+//let firstCity;
+//let secondCity = firstCity || "London"
 //console.log(`City: ${secondCity}`)
-console.log(`City: ${firstCity || "London"}`)
+//console.log(`City: ${firstCity || "London"}`)
 
 
-let taxRate; // no tax rate has been defined
-console.log(`Tax rate: ${taxRate ?? 10}%`)
-taxRate = 0
-console.log(`Tax rate: ${taxRate ?? 10}%`)
+//let taxRate; // no tax rate has been defined
+//console.log(`Tax rate: ${taxRate ?? 10}%`)
+//taxRate = 0
+//console.log(`Tax rate: ${taxRate ?? 10}%`)
